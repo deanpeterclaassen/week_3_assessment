c4bb0f356cbbd1d9915e174c2604c7da4528c3a8
DeanPeter Claassen <deanpeter8162@gmail.com>
2022-09-30 10:59:09 -0400

    Added parts 1-3

---
+++ about.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Form Submitted Successfully!');
+}
+
+function imgAlert(evt){
+	evt.preventDefault();
+	alert("I like your style ;)")
+}
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+let img = document.querySelector('img')
+
+img.addEventListener("mouseover", imgAlert)
+
